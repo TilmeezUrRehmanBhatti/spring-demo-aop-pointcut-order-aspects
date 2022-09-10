fa59b2df4de7c4202e5fc23cf533926c4a152f6b
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-10 16:18:58 +0200

    Refactor: Placed advices in separate Aspects

--- src/com/tilmeez/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ src/com/tilmeez/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,16 +2,14 @@ package com.tilmeez.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class MyDemoLoggingAspect {
+public class MyCloudLogAsyncAspect {
 
     @Before("com.tilmeez.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n====>>> Executing @Before advice on addAccount");
+    public void logToCloudAsync() {
+        System.out.println("\n====>>> Logging to Cloud  ");
     }
-
 }
